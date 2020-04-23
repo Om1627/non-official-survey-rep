20b468126075c89b6f0ab0e5f627087af62ee447
Om1627 <60969676+Om1627@users.noreply.github.com>
2020-04-23 15:47:25 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var canvas,backgroundImage;
+var voterCount=0;
+var database;
+var form,im;
+
+
+
+function setup(){
+  
+ createCanvas(400,400);
+form=new Form();
+
+}
+
+function draw(){
+  
+  form.display();
+ 
+  
+}
+
+
